36b1c4551b4ee5e405c57685f841e8ff027d5485
Kungusto <specifikbrateevo@gmail.com>
2025-05-16 14:43:04 +0300

    добавление фикстур и первого пробного теста. настройка pytest-dotenv

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,9 +1,22 @@
 import pytest
 
+import sys
+from pathlib import Path
+
+sys.path.append(str(Path(__file__).parent.parent))
+
 from src.database import async_session_maker_null_pool
 from src.utils.dbmanager import DBManager
-from src.config import settings
+from src.config import settings, Settings
 
-@pytest.fixture(scope="session")
-def check_mode() :
+from dotenv import load_dotenv
+import os
+
+os.environ["MODE"] = "TEST"
+load_dotenv(".env-test", override=True)
+
+settings = Settings()
+
+@pytest.fixture(scope="session", autouse=True)
+async def setup_database():
     assert settings.MODE == "TEST"

13cc67216c4f153d7f57df908389735141844ab5
Kungusto <specifikbrateevo@gmail.com>
2025-06-04 11:49:24 +0300

    добавление api теста

--- tests/api/auth/test_api.py
+++ tests/api/auth/test_api.py
@@ -2,9 +2,23 @@ from aiohttp import ClientSession
 
 async def test_api_registration():
     async with ClientSession() as session : 
-        # регистрация
+        response = await session.post(
+            url="http://127.0.0.1:8000/auth/register",
+            json={
+                "role": "USER",
+                "email": "duck_in_the_truck@gmail.com",
+                "name": "Lidia",
+                "surname": "Adams",
+                "nickname": "LidiaDuck12",
+                "password": "Duck_panda_L98"
+            }
+        )
+        assert response.status == 200
         # логин
         # данные о себе
         # данные о другом пользователе
         # разлогин
         ...
+
+
+        

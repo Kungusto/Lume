be529b17250a7d77fc052590f170f121f1b0d80a
Kungusto <specifikbrateevo@gmail.com>
2025-07-13 13:29:09 +0300

    fix(factories): корректная генерация уникальных названий жанров

--- tests/factories/genres_factory.py
+++ tests/factories/genres_factory.py
@@ -1,7 +1,11 @@
-from tests.fake_data.genres import BOOK_GENRES
+import random
 from src.schemas.books import GenreAdd
 import factory
 from faker import Faker
+
+# заранее заготовленный список жанров
+from tests.fake_data.genres import BOOK_GENRES 
+
 faker = Faker("ru_RU")
 
 
@@ -9,4 +13,12 @@ class GenreAddFactory(factory.Factory):
     class Meta:
         model = GenreAdd
 
-    title = factory.LazyAttribute(lambda _: faker.unique.word(ext_word_list=BOOK_GENRES))
+    title = factory.LazyAttribute(lambda _: generate_genre_title(BOOK_GENRES))
+
+
+def generate_genre_title(BOOK_GENRES):
+    # проверяем, не пуст ли список жанров
+    if not BOOK_GENRES:
+        return faker.unique.word()
+    else:
+        return BOOK_GENRES.pop(random.randrange(len(BOOK_GENRES)))

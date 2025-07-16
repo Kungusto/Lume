0df3e6e8d02072009f95405d4c4a01bf0e28562f
Kungusto <specifikbrateevo@gmail.com>
2025-07-16 12:57:47 +0300

    chore(config): добавлено AUTO_STATEMENT_PATH и обновлены комментарии

--- src/config.py
+++ src/config.py
@@ -4,20 +4,30 @@ from typing import Literal
 
 
 class Settings(BaseSettings):
+    """
+    Режимы работы:
+    - LOCAL: локальная разработка
+    - TEST: выполнение тестов
+    - PROD: запуск на боевом сервере
+    """
     MODE: Literal["LOCAL", "TEST", "PROD"]
 
+    # Базы данных
     DB_USER: str
     DB_PASS: str
     DB_HOST: str
     DB_PORT: int
     DB_NAME: str
 
+    # JWT
     JWT_SECRET_KEY: str
     JWT_ALGORITHM: str
 
+    # Redis
     REDIS_HOST: str
     REDIS_PORT: int
 
+    # S3 Хранилище
     S3_BUCKET_NAME: str
     S3_STATIC_BUCKET_NAME: str  # бакет с контетном для тестов
     S3_REGION: str
@@ -25,19 +35,26 @@ class Settings(BaseSettings):
     S3_ACCESS_KEY: str
     S3_SECRET_KEY: str
 
+    # Аналитика
+    AUTO_STATEMENT_PATH: str
+
+
+    # Асинхронное подключение
     @property
     def DB_URL(self):
         return f"postgresql+asyncpg://{self.DB_USER}:{self.DB_PASS}@{self.DB_HOST}:{self.DB_PORT}/{self.DB_NAME}"
 
-    # синхронное подключение (только для Celery)
+    # Синхронное подключение (только для Celery)
     @property
     def DB_URL_SYNC(self):
         return f"postgresql://{self.DB_USER}:{self.DB_PASS}@{self.DB_HOST}:{self.DB_PORT}/{self.DB_NAME}"
 
+    # URL подключения к S3
     @property
     def S3_URL(self):
         return f"https://s3.{self.S3_REGION}.{self.S3_DOMAIN}"
 
+    # URL подключения к Redis
     @property
     def REDIS_URL(self):
         return f"redis://{self.REDIS_HOST}:{self.REDIS_PORT}"

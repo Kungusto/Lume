3c99c41d64ae6df6b622f751178f68dcd2b808fa
Kungusto <specifikbrateevo@gmail.com>
2025-07-18 14:50:19 +0300

    perf(cache): оптимизация обработки json
    Заменение `json` на более быстрый вариант,
    написанный на Rust - `orjson`

--- src/services/cache/base.py
+++ src/services/cache/base.py
@@ -1,22 +1,23 @@
 from datetime import timedelta
-import json
+import orjson
 from typing import Any, Dict
 
-
 class BaseCacheService:
-    def __init__(self, redis): 
+    def __init__(self, redis):
         self.redis = redis
 
     async def _get(self, key: str) -> Dict[str, Any] | None:
         cached_data = await self.redis.get(key)
-        return json.loads(cached_data) if cached_data else None
+        return orjson.loads(cached_data) if cached_data else None
 
     async def _set(self, key: str, value: Dict[str, Any], ttl: int = 3600) -> None:
         await self.redis.setex(
-            key,
-            timedelta(seconds=ttl),
-            json.dumps(value)
-        )
+            key, 
+            timedelta(seconds=ttl), 
+            orjson.dumps(
+                value, 
+                option=orjson.OPT_NAIVE_UTC | orjson.OPT_OMIT_MICROSECONDS)
+            )
 
     async def _invalidate(self, key: str):
         await self.redis.delete(key)

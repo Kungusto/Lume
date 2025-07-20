c5d75deeba01e63b2353b4d9b97f916c3d3a370c
Kungusto <specifikbrateevo@gmail.com>
2025-07-20 19:53:08 +0300

    feat(repositories): метод для поиска бана пользователя
    Добавлен метод get_ban_by_user_id() в BansRepository,
    Для точечного поиска активных банов пользователя

--- src/repositories/database/reports.py
+++ src/repositories/database/reports.py
@@ -28,6 +28,26 @@ class BansRepository(BaseRepository):
             for user, ban_until in models.all()
         ]
 
+    async def get_ban_by_user_id(self, user_id: int) -> UserWithBanDate | None:
+        now = datetime.now(timezone.utc)
+        query = (
+            select(UsersORM, func.max(BanORM.ban_until))
+            .select_from(BanORM)
+            .filter(BanORM.ban_until > now)
+            .filter(BanORM.user_id == user_id)
+            .join(UsersORM, UsersORM.user_id == BanORM.user_id)
+            .group_by(UsersORM.user_id)
+        )
+        model = await self.session.execute(query)
+        row = model.one_or_none()
+        if row is None:
+            return None
+        user, ban_until = row
+        return UserWithBanDate(
+            **User.model_validate(user, from_attributes=True).model_dump(),
+            ban_until=ban_until,
+        )
+
 
 class ReasonsRepository(BaseRepository):
     model = ReasonsORM

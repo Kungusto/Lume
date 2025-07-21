60090f3f9b1f262667ec10d067c76366e3c54bac
Kungusto <specifikbrateevo@gmail.com>
2025-07-21 19:17:42 +0300

    perf(books): увеличение скорости получения страниц в x23 раза
    - Раньше вытаскивался весь .pdf документ, парсился на изображения
    и текст, что занимало до 26 с. на получения одной страницы
    - Теперь весь текст и путь изображений запоминается на этапе рендера

--- src/api/books.py
+++ src/api/books.py
@@ -105,11 +105,6 @@ async def get_page(
         raise ContentNotFoundHTTPException
     if (book.total_pages is None) or (page_number > book.total_pages):
         raise PageNotFoundHTTPException(page_number=page_number)
-    book_file = await s3.books.get_file_by_path(f"books/{book_id}/book.pdf")
-    doc = fitz.open(stream=book_file, filetype="pdf")
-    content_data = PDFRenderer.parse_text_end_images_from_page(
-        page_number=page_number, book_id=book_id, doc=doc
-    )
     user_read_book = await db.user_reads.get_filtered(user_id=user_id, book_id=book_id)
     if not user_read_book:
         await db.user_reads.add(
@@ -125,12 +120,24 @@ async def get_page(
         )
     await db.commit()
 
-    for block in content_data:
-        if block["type"] == "image":
-            image_path = block["path"]
-            new_path = await s3.books.generate_url(file_path=image_path)
-            block["path"] = new_path
-    return content_data
+
+    page = await db.pages.get_one(book_id=book_id, page_number=page_number)
+    page_content = page.content
+    # Убрать лишние пробелы из текста
+    for ind, content_info in enumerate(page_content):
+        if content_info["type"] == "image":
+            path_in_s3 = content_info["path"]
+            access_url = await s3.books.generate_url(
+                file_path=path_in_s3,
+                expires_in=60*60*24 # 1 день
+            )
+            content_info["path"] = access_url
+        else:
+            content_info["content"] = TextFormatingManager.replace_nbsp(content_info["content"])
+        page_content[ind] = content_info
+
+    page.content = page_content
+    return page
 
 
 @router.post("/{book_id}/report")
